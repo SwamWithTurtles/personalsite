91d42299cd5095f5594181dc820bd7706b8569ee
David Simons <12e.david@gmail.com>
2016-11-28 00:15:48 +0000

    Update static assets

--- br/Brexit_ voter turnout by age _ FT Data_files/ads.js
+++
@@ -1,1 +0,0 @@
-window.detectedAdBlock=false
--- br/Brexit_ voter turnout by age _ FT Data_files/script.js
+++
@@ -1,5 +0,0 @@
-/* Javascript for the FT Wrapper plugin. */
-
-jQuery(function($) {
-	$("#ftLogin-box input[name=location]").val(window.location.href);
-});
